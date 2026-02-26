ab2f1c59e4d8f1195205e7210a0687b49d29f4f1
chandrasekhar <chandrasekhar.g@dhanushsoftech.com>
2026-02-26 14:54:47 +0530

    url-issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,16 @@
+let apiURL: string = location.origin.includes("localhost") ? "https://oukinternship.dhanushinfotech.com" : location.origin;
+
 export const environment = {
   production: true,
   // serviceUrl: "http://10.10.11.74:8090/api/",
-  serviceUrl: "https://shiksion.com/api/",
+  serviceUrl: `${apiURL}/api/`,
   internUrl: "https://ouk.samvaadpro.com/api/",
   // serviceUrl:"https://lmsqa.dhanushinfotech.com/api/",
-  fileUrl: "https://shiksion.com/",
-  urlFiles: "https://shiksion.com",
+  fileUrl: `${apiURL}/`,
+  urlFiles: `${apiURL}`,
   internUrlFiles: "https://ouk.samvaadpro.com",
-  reportUrl: "https://shiksion.com/internship/v1/generate",
-  downloadReportUrl: "https://shiksion.com/internship/v1/download/",
+  reportUrl: `${apiURL}/internship/v1/generate`,
+  downloadReportUrl: `${apiURL}/internship/v1/download/`,
   SOCKET_ENDPOINT: 'http://localhost:3000',
   SamvaadUrl: 'https://scheduling.samvaad.pro/conference/',
   // RazorPay_Key: 'rzp_test_n9kOog8GmBqfim'//Test
